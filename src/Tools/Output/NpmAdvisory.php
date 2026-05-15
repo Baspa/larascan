efd47d3d47011d0fd54f17d77648e8f6c3c59764
Baspa <hello@baspa.dev>
2026-05-15 16:45:27 +0200

    feat: add NpmAuditRunner with advisory parsing

---
+++ src/Tools/Output/NpmAdvisory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baspa\Larascan\Tools\Output;
+
+final readonly class NpmAdvisory
+{
+    public function __construct(
+        public string $packageName,
+        public string $title,
+        public string $severity,
+        public ?string $range = null,
+        public ?string $url = null,
+    ) {
+    }
+}
